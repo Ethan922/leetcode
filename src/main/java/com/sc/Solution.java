be55c9242ced487e55100506e8bc209e21aa5710
Ethan <2553035342@qq.com>
2025-07-20 02:18:34 +0800

    feat:只出现一次的数字题解

--- src/main/java/com/sc/Solution.java
+++ src/main/java/com/sc/Solution.java
@@ -801,11 +801,20 @@ public class Solution {
                 if (max >= nums.length - 1) {
                     return true;
                 }
-            }else {
+            } else {
                 break;
             }
         }
         return false;
+    }
+
+    // 只出现一次的数字
+    public int singleNumber(int[] nums) {
+        int ret = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            ret ^= nums[i];
+        }
+        return ret;
     }
 
     public static void main(String[] args) {
