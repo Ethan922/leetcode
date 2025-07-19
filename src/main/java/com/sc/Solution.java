a8387d88112c7b25bce9342dd67b2ee093881293
Ethan <2553035342@qq.com>
2025-07-19 19:14:43 +0800

    feat:旋转矩阵题解

--- src/main/java/com/sc/Solution.java
+++ src/main/java/com/sc/Solution.java
@@ -550,6 +550,30 @@ public class Solution {
         return list;
     }
 
+    // 旋转矩阵
+    public void rotate(int[][] matrix) {
+        // 水平翻转
+        int up = 0;
+        int down = matrix.length - 1;
+        while (up < down){
+            for (int j = 0; j < matrix[0].length; j++) {
+                int temp=matrix[up][j];
+                matrix[up][j]=matrix[down][j];
+                matrix[down][j]=temp;
+            }
+            up++;
+            down--;
+        }
+        // 主对角线翻转
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < i; j++) {
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println(maxSubArrayDP(new int[]{5, 4, -1, 7, 8}));
 //        System.out.println(maxSubArray(new int[]{5, 4, -1, 7, 8}));
