7c797cb87e87f75c9bc4c90821a2d7da9819036a
Ethan <2553035342@qq.com>
2025-07-20 00:39:19 +0800

    feat:环形链表题解

--- src/main/java/com/sc/Solution.java
+++ src/main/java/com/sc/Solution.java
@@ -648,6 +648,23 @@ public class Solution {
         return true;
     }
 
+    // 环形链表
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow=slow.next;
+            fast=fast.next.next;
+            if (slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         System.out.println(maxSubArrayDP(new int[]{5, 4, -1, 7, 8}));
 //        System.out.println(maxSubArray(new int[]{5, 4, -1, 7, 8}));
