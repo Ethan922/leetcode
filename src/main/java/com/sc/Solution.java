7024916efac794821cba294bfd7593c8b1c94358
Ethan <2553035342@qq.com>
2025-07-21 18:51:58 +0800

    feat:在排序数组中查找元素的第一个和最后一个位置题解

--- src/main/java/com/sc/Solution.java
+++ src/main/java/com/sc/Solution.java
@@ -851,6 +851,30 @@ public class Solution {
         return count;
     }
 
+    // 二分查找（查找有序数组中第一个大于等于target的元素下标）
+    public int binSearch(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return l;
+    }
+
+    // 在排序数组中查找元素的第一个和最后一个位置
+    public int[] searchRange(int[] nums, int target) {
+        int first = binSearch(nums, target);
+        if (first == nums.length || nums[first] != target) {
+            return new int[]{-1, 1};
+        }
+        int last = binSearch(nums, target + 1) - 1;
+        return new int[]{first, last};
+    }
+
     public static void main(String[] args) {
         minSubArrayLen(7, new int[]{2, 3, 1, 2, 4, 3});
 //        System.out.println(maxSubArrayDP(new int[]{5, 4, -1, 7, 8}));
