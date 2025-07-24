a4c996b7325300db3a71e0ab0de76fc8946fd2c2
Ethan <2553035342@qq.com>
2025-07-24 13:13:38 +0800

    feat:验证二叉搜索树 递归写法

--- src/main/java/com/sc/Solution.java
+++ src/main/java/com/sc/Solution.java
@@ -1181,6 +1181,21 @@ public class Solution {
         return Math.max(l, r) + 1;
     }
 
+    // 验证二叉搜索树 递归写法
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean isValidBST(TreeNode node, long lower, long upper) {
+        if (node == null) {
+            return true;
+        }
+        if (node.val <= lower || node.val >= upper) {
+            return false;
+        }
+        return isValidBST(node.left, lower, node.val) && isValidBST(node.right, node.val, upper);
+    }
+
 
     public static void main(String[] args) {
         minSubArrayLen(7, new int[]{2, 3, 1, 2, 4, 3});
