aaac11737ea17726e3abfe91a777528870e4ee87
Ethan <2553035342@qq.com>
2025-07-24 11:36:25 +0800

    feat:对称二叉树递归写法

--- src/main/java/com/sc/Solution.java
+++ src/main/java/com/sc/Solution.java
@@ -1126,6 +1126,21 @@ public class Solution {
         return ans;
     }
 
+    // 对称二叉树 递归写法
+    public boolean isSymmetric(TreeNode root) {
+        return check(root.left, root.right);
+    }
+
+    private boolean check(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        return left.val == right.val && check(left.left, left.right) && check(right.left, right.right);
+    }
+
 
     public static void main(String[] args) {
         minSubArrayLen(7, new int[]{2, 3, 1, 2, 4, 3});
