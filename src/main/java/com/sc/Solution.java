5cf8c00d98f9874d01426fc64d841771593b4440
Ethan <2553035342@qq.com>
2025-07-24 10:31:46 +0800

    feat:二叉树的最大深度迭代写法

--- src/main/java/com/sc/Solution.java
+++ src/main/java/com/sc/Solution.java
@@ -1041,6 +1041,31 @@ public class Solution {
         return root == null ? 0 : Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
 
+    // 二叉树的最大深度 迭代写法
+    public int maxDepthIteration(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int ans = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size > 0) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                size--;
+            }
+            ans++;
+        }
+        return ans;
+    }
+
 
     public static void main(String[] args) {
         minSubArrayLen(7, new int[]{2, 3, 1, 2, 4, 3});
