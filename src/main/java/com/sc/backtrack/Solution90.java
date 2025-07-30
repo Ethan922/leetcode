7c75f09ac84b6d948c3c3405f4b85ec17adf7e35
Ethan <2553035342@qq.com>
2025-07-30 12:26:20 +0800

    feat:子集II

---
+++ src/main/java/com/sc/backtrack/Solution90.java
@@ -0,0 +1,29 @@
+package com.sc.backtrack;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution90 {
+    // 子集II
+    List<List<Integer>> ans = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        backTracking(nums, 0);
+        return ans;
+    }
+
+    private void backTracking(int[] nums, int startIndex) {
+        ans.add(new ArrayList<>(path));
+        for (int i = startIndex; i < nums.length; i++) {
+            if (i > startIndex && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            path.add(nums[i]);
+            backTracking(nums, i + 1);
+            path.remove(path.size() - 1);
+        }
+    }
+}
