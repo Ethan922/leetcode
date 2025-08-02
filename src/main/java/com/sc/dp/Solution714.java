b1362147e50a1609cecf6082a4d3493a136c3937
Ethan <2553035342@qq.com>
2025-08-02 16:58:09 +0800

    feat:买卖股票的最佳时机含手续费

---
+++ src/main/java/com/sc/dp/Solution714.java
@@ -0,0 +1,19 @@
+package com.sc.dp;
+
+public class Solution714 {
+    // 买卖股票的最佳时机含手续费
+    public int maxProfit(int[] prices, int fee) {
+        int[][] dp = new int[prices.length][2];
+        // 持有股票
+        dp[0][0] = -prices[0];
+        // 不持有股票
+        dp[0][1] = 0;
+        for (int i = 1; i < prices.length; i++) {
+            // 持有股票
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
+            // 不持有股票
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i] - fee);
+        }
+        return dp[prices.length - 1][1];
+    }
+}
