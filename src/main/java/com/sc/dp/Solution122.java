2aeef7db4b4c53c5fc5debf098d2cc4de8a4043b
Ethan <2553035342@qq.com>
2025-08-02 17:05:27 +0800

    feat:买卖股票的最佳时机II

---
+++ src/main/java/com/sc/dp/Solution122.java
@@ -0,0 +1,20 @@
+package com.sc.dp;
+
+public class Solution122 {
+    // 买卖股票的最佳时机II
+    public int maxProfit(int[] prices) {
+        int[][] dp = new int[prices.length][2];
+        // 持有
+        dp[0][0] = -prices[0];
+        // 不持有
+        dp[0][1] = 0;
+        for (int i = 1; i < prices.length; i++) {
+            // 持有
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
+            // 不持有
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+        }
+        return dp[prices.length - 1][1];
+    }
+}
+
