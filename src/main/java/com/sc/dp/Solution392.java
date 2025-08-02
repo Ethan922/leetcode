9c181af750c2afc5effcd313f6b5144dacd6a55f
Ethan <2553035342@qq.com>
2025-08-03 04:02:46 +0800

    feat:判断子序列DP

--- src/main/java/com/sc/dp/Solution392.java
+++ src/main/java/com/sc/dp/Solution392.java
@@ -1,7 +1,7 @@
 package com.sc.dp;
 
 public class Solution392 {
-    // 最长公共子序列
+    // 判断子序列
     public boolean isSubsequence(String s, String t) {
         char[] chars1 = s.toCharArray();
         char[] chars2 = t.toCharArray();
@@ -21,6 +21,21 @@ public class Solution392 {
             }
         }
         return false;
+    }
+
+    public boolean isSubsequenceDP(String s, String t) {
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        int m = sChars.length;
+        int n = tChars.length;
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (sChars[i - 1] == tChars[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
+                else dp[i][j] = dp[i][j - 1];
+            }
+        }
+        return dp[m][n] == m;
     }
 }
 
