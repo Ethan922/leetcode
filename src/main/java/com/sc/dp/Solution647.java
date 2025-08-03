9bd6ddbe516ebe670ccc6c0f8228258af576fe71
Ethan <2553035342@qq.com>
2025-08-03 17:28:39 +0800

    feat:回文子串动态规划解法

--- src/main/java/com/sc/dp/Solution647.java
+++ src/main/java/com/sc/dp/Solution647.java
@@ -2,6 +2,34 @@ package com.sc.dp;
 
 public class Solution647 {
     // 回文子串
+
+    public int countSubstringsDP(String s) {
+        char[] chars = s.toCharArray();
+        int n = chars.length;
+        // [i,j]的子串是否为回文子串
+        boolean[][] dp = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
+        int ans = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                if (chars[i] == chars[j]) {
+                    if (j - i <= 1) {
+                        ans++;
+                        dp[i][j] = true;
+                    } else {
+                        if (dp[i + 1][j - 1]){
+                            ans++;
+                            dp[i][j] = true;
+                        }
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
     public int countSubstrings(String s) {
         int n = s.length();
         int count = 0;
