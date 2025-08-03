f50b9e7402e221dfb2ee6757e8fedf7105508c62
Ethan <2553035342@qq.com>
2025-08-03 18:15:27 +0800

    feat:最小栈

---
+++ src/main/java/com/sc/MinStack.java
@@ -0,0 +1,40 @@
+package com.sc;
+
+import java.util.Stack;
+
+public class MinStack {
+
+    Stack<int[]> stack;
+
+    public MinStack() {
+        stack = new Stack<>();
+    }
+
+    public void push(int val) {
+        if (stack.isEmpty()) {
+            stack.push(new int[]{val, val});
+        } else {
+            stack.push(new int[]{val, Math.min(stack.peek()[1], val)});
+        }
+    }
+
+    public void pop() {
+        if (!stack.isEmpty()) {
+            stack.pop();
+        }
+    }
+
+    public int top() {
+        if (!stack.isEmpty()) {
+            return stack.peek()[0];
+        }
+        return 0;
+    }
+
+    public int getMin() {
+        if (!stack.isEmpty()) {
+            return stack.peek()[1];
+        }
+        return Integer.MAX_VALUE;
+    }
+}
