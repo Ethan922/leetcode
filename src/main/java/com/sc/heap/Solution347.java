21da0286cda11601c610cb718d480953ce11e418
Ethan <2553035342@qq.com>
2025-08-22 13:54:45 +0800

    feat:前K个高频元素

--- src/main/java/com/sc/heap/Solution347.java
+++ src/main/java/com/sc/heap/Solution347.java
@@ -11,22 +11,23 @@ public class Solution347 {
         for (int num : nums) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        PriorityQueue<int[]> queue = new PriorityQueue<>((int[] o1, int[] o2) -> o1[1] - o2[1]);
+        PriorityQueue<int[]> queue = new PriorityQueue<>((o1, o2) -> o1[0] != o2[0] ? o1[0] - o2[0] : o1[1] - o2[1]);
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            Integer num = entry.getKey();
             Integer count = entry.getValue();
+            Integer num = entry.getKey();
             if (queue.size() == k) {
-                if (count > queue.peek()[1]) {
+                if (count > queue.peek()[0]) {
                     queue.poll();
-                    queue.offer(new int[]{num, count});
+                    queue.offer(new int[]{count, num});
                 }
             } else {
-                queue.offer(new int[]{num, count});
+                queue.offer(new int[]{count, num});
             }
         }
         int[] ans = new int[k];
-        for (int i = 0; i < k; i++) {
-            ans[i] = queue.poll()[0];
+        int i = 0;
+        for (int[] arr : queue) {
+            ans[i++] = arr[1];
         }
         return ans;
     }
