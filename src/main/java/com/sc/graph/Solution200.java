460e88de416253e6f859d3e4138e313944978931
Ethan <2553035342@qq.com>
2025-08-23 00:57:22 +0800

    feat:岛屿数量（广度优先遍历）

--- src/main/java/com/sc/graph/Solution200.java
+++ src/main/java/com/sc/graph/Solution200.java
@@ -1,6 +1,11 @@
 package com.sc.graph;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Solution200 {
+    int[][] directions = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
     // 岛屿数量
     public int numIslands(char[][] grid) {
         int ans = 0;
@@ -10,21 +15,40 @@ public class Solution200 {
             for (int j = 0; j < col; j++) {
                 if (grid[i][j] == '1') {
                     ans++;
-                    dfs(grid, i, j);
+                    bfs(grid, i, j);
                 }
             }
         }
         return ans;
     }
 
+    // 广度优先遍历
+    public void bfs(char[][] grid, int i, int j) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{i, j});
+        grid[i][j] = '0';
+        while (!queue.isEmpty()) {
+            int[] index = queue.poll();
+            for (int k = 0; k < 4; k++) {
+                int nextI = index[0] + directions[k][0];
+                int nextJ = index[1] + directions[k][1];
+                if (nextI < 0 || nextJ < 0 || nextI == grid.length || nextJ == grid[0].length || grid[nextI][nextJ] == '0') {
+                    continue;
+                }
+                grid[nextI][nextJ] = '0';
+                queue.offer(new int[]{nextI, nextJ});
+            }
+        }
+    }
+
+    // 深度优先遍历
     public void dfs(char[][] grid, int i, int j) {
-        if (i < 0 || j < 0 || i == grid.length || j == grid[0].length || grid[i][j]=='0') {
+        if (i < 0 || j < 0 || i == grid.length || j == grid[0].length || grid[i][j] == '0') {
             return;
         }
         grid[i][j] = '0';
-        dfs(grid, i - 1, j);
-        dfs(grid, i + 1, j);
-        dfs(grid, i, j + 1);
-        dfs(grid, i, j - 1);
+        for (int k = 0; k < 4; k++) {
+            dfs(grid, i + directions[i][0], j + directions[i][1]);
+        }
     }
 }
